aa3016e86d5b34e082eb5b13d5d6a30fe6b40fac
attero <code@attero.me>
2020-11-19 15:34:55 +0100

    bruh

---
+++ Packages/User/Extension_RevertFontSize.py
@@ -0,0 +1,39 @@
+import sublime
+import sublime_plugin
+
+# src https://github.com/SublimeText/RevertFontSize
+
+def is_sublime_text_3():
+    """Returns True if this plugin is currently being run in Sublime Text 3."""
+    try:
+        return int(sublime.version()) >= 3000
+    except ValueError:
+        return sys.hexversion >= 0x030000F0
+
+def plugin_loaded():
+    """Called when this plugin is loaded; Sublime Text 3 API."""
+    preferences = sublime.load_settings("Preferences.sublime-settings")
+    if not preferences.get("revert_font_size"):
+        default_font_size = preferences.get("font_size", 10)
+        preferences.set("revert_font_size", default_font_size)
+        sublime.save_settings("Preferences.sublime-settings")
+
+def plugin_unloaded():
+    """Called when this plugin is unloaded; Sublime Text 3 API."""
+    pass
+
+class RevertFontSizeCommand(sublime_plugin.ApplicationCommand):
+    """Plugin to revert user 'font_size' to a 'default' value."""
+    def run(self):
+        preferences = sublime.load_settings("Preferences.sublime-settings")
+        revert_font_size = preferences.get("revert_font_size", 10)    # value 10 is the fallback, just in case
+        preferences.set("font_size", revert_font_size)
+        sublime.save_settings("Preferences.sublime-settings")
+
+class SetRevertFontSizeValueCommand(sublime_plugin.ApplicationCommand):
+    def run(self):
+        preferences = sublime.load_settings("Preferences.sublime-settings")
+        font_size = preferences.get("font_size")
+        preferences.set("revert_font_size", font_size)
+        sublime.save_settings("Preferences.sublime-settings")
+        sublime.status_message("revert_font_size value is set to " + str(font_size))
